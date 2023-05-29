31a8a7dc8a7b7f75d90fd070c7b257adbb108e0c
Pedro Soldera <113148885+PedroSoldera@users.noreply.github.com>
2023-05-29 20:58:01 -0300

    7- Exercicio 3 - Lista 2.c

---
+++ 7- Exercicio 3 - Lista 2.c
@@ -0,0 +1,30 @@
+/******************************************************************************
+ * Exercicio 3 
+Pedro Soldera dos Santos - RA: 8135417
+
+Leia um vetor de 10 posi��es
+conte e escreva quantos valors pares ele possui.
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main(){
+
+    int valores[10], i , numPar = 0;
+    
+    printf ("Digite 10 valores inteiros : \n") ;
+        for (i =0 ; i < 10 ; i++){
+            scanf ("%d", &valores[i]);
+        }
+    
+    for (i = 0; i < 10; i++){
+        if (valores [i] % 2 == 0){
+            numPar++;
+        }
+    }
+    
+    printf ("\nExistem: %d numeros pares", numPar);
+ 
+    return 0;
+}
+
