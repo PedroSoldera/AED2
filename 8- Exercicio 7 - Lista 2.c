896ff7c6e649d5def72076c791d841038c02ff00
Pedro Soldera <113148885+PedroSoldera@users.noreply.github.com>
2023-05-29 20:59:54 -0300

    8- Exercicio 7 - Lista 2.c

---
+++ 8- Exercicio 7 - Lista 2.c
@@ -0,0 +1,44 @@
+/******************************************************************************
+
+Pedro Soldera dos Santos - RA: 8135417
+
+01/03/2023
+
+Fa�a um programa que preencha um vetor com 10 numeros reais.
+Calcule e mostre:
+
+a) Soma dos numeros positivos desse vetor.
+
+b) Quantidade de negativos
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+    int cont, quantNegativo = 0;
+    float vetor[10],somaPositivo = 0;
+    
+        printf("Digite 10 numeros Reais : \n");
+        
+            for(cont = 0; cont < 10;  cont++){
+                scanf("%f", &vetor[cont]);
+                
+                if(vetor[cont] < 0 ){
+                    quantNegativo++;
+                }
+                
+                if(vetor[cont] > 0 ){
+                    somaPositivo += vetor[cont];
+                }
+                
+            
+            }
+
+            printf("Quantidade negativos: %d\nSoma dos positvos: %.2f",quantNegativo,somaPositivo);
+
+    return 0;
+}
+
+
+
