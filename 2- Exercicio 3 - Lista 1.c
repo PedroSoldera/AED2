31c3c05320682540b96880ccbfeb10564617bbf6
Pedro Soldera <113148885+PedroSoldera@users.noreply.github.com>
2023-05-29 20:50:01 -0300

    Create 2- Exercicio 3 - Lista 1.c

---
+++ 2- Exercicio 3 - Lista 1.c
@@ -0,0 +1,33 @@
+/******************************************************************************
+
+Pedro Soldera dos Santos - RA: 8135417
+
+15/02/2023
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+    int carros; 
+    float valor, salario, comissao;
+    
+    printf("Digite o salario do funcionario: \n");
+    scanf("%f", &salario);
+    
+    printf("Digite o numero de carros vendidos: \n");
+    scanf("%d", &carros);
+    
+    printf("Digite o valor total dos carros vendidos: \n");
+    scanf("%f", &valor);
+    
+    printf("Digite a comissao por carro: \n");
+    scanf("%f", &comissao);
+    
+    salario = salario + carros * comissao + 0.05 * valor;
+    
+    printf("O salario total do funcionario e: %.1f", salario);
+
+    return 0;
+}
+
